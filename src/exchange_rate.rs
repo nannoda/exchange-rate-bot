642be849fa46ac4cdfb251c4f980bc9f258aaa95
Nan Noda <114621472+nannoda@users.noreply.github.com>
2025-01-08 15:56:08 -0600

    Update exchange_rate.rs

--- src/exchange_rate.rs
+++ src/exchange_rate.rs
@@ -4,7 +4,7 @@ use thiserror::Error;
 
 use crate::{
     database::historical_data::get_historical_exchange_rate_db,
-    environment::DAYS_TO_CHECK,
+    environment::{self, DAYS_TO_CHECK},
     exchange_rate_api::{fetch_exchange_rate, FetchExchangeRateError, FetchMode},
 };
 use std::{collections::HashMap, fmt};
@@ -78,71 +78,23 @@ impl ExchangeRateMap {
         if to_val.is_none() {
             return None;
         }
-
-        // If both values are present, calculate the exchange rate
         let from_value = from_val.unwrap();
         let to_value = to_val.unwrap();
         Some(to_value / from_value)
     }
+}
 
-    // Constructor that manually parses the JSON into an ExchangeRate struct
-    pub fn from_json(json: &str) -> Result<Self, ExchangeRateError> {
-        let parsed: Value = match serde_json::from_str(json) {
-            Ok(value) => value,
-            Err(e) => return Err(ExchangeRateError::ParseError(e)),
-        };
+pub struct ExchangeRateHistory {
+    pub rates: ExchangeRateMap,
+}
 
-        // Check for 'success' field first, and handle possible API errors
-        if let Some(error_info) = parsed.get("error") {
-            if let (Some(code), Some(info)) = (error_info.get("code"), error_info.get("info")) {
-                if let (Some(code_str), Some(info_str)) = (code.as_u64(), info.as_str()) {
-                    return Err(ExchangeRateError::ApiError(
-                        code_str.to_string(),
-                        info_str.to_string(),
-                    ));
-                }
-            }
-        }
+impl ExchangeRateHistory {
+    pub fn create(last: DateTime<Utc>) -> ExchangeRateHistory {
+        let api_url = environment::get_exchange_rate_api_url();
+        let fetch_url = format!("{}/{}", api_url, last.format("%Y-%m-%d"));
 
-        let date = parsed
-            .get("date")
-            .and_then(|t| t.as_str())
-            .and_then(|date_str| {
-                Some(
-                    NaiveDate::parse_from_str(date_str, "%Y-%m-%d")
-                        .map_err(|_| ExchangeRateError::ParseDateError),
-                )
-            })
-            .ok_or_else(|| ExchangeRateError::MissingField("date".to_string()))??;
+        
 
-        let base = parsed
-            .get("base")
-            .and_then(|b| b.as_str())
-            .ok_or_else(|| ExchangeRateError::MissingField("base currency".to_string()))?
-            .to_string();
-
-        let rates = parsed
-            .get("rates")
-            .and_then(|r| r.as_object())
-            .ok_or_else(|| ExchangeRateError::MissingField("rates".to_string()))?;
-
-        // Convert the rates into a HashMap
-        let mut rate_map = HashMap::new();
-        for (currency, rate_value) in rates {
-            if let Some(rate) = rate_value.as_f64() {
-                rate_map.insert(currency.to_string(), rate);
-            } else {
-                return Err(ExchangeRateError::InvalidData(format!(
-                    "Invalid rate data for currency: {}",
-                    currency
-                )));
-            }
-        }
-        Ok(ExchangeRateMap {
-            base,
-            date,
-            map: rate_map,
-        })
     }
 }
 
@@ -156,42 +108,4 @@ pub enum LocalExchangeRateError {
 
 pub enum GetRatesError {
     RemoteError(FetchExchangeRateError),
-}
-
-pub async fn get_exchange_rate_from_date(
-    date: &NaiveDate,
-) -> Result<ExchangeRateMap, GetRatesError> {
-    match get_historical_exchange_rate_db(date) {
-        Some(map) => return Ok(map),
-        None => log::debug!("Cannot find historical data on {}", date),
-    };
-    match fetch_exchange_rate(FetchMode::Date(date)).await {
-        Ok(map) => return Ok(map),
-        Err(err) => return Err(GetRatesError::RemoteError(err)),
-    }
-}
-
-pub async fn get_exchange_rates() -> Result<Vec<ExchangeRateMap>, GetRatesError> {
-    let mut rates = Vec::new();
-
-    // Get the current exchange rate
-    let latest_date = match fetch_exchange_rate(FetchMode::Latest).await {
-        Ok(map) => {
-            let date = map.date.clone();
-            rates.push(map);
-            date
-        },
-        Err(err) => return Err(GetRatesError::RemoteError(err)),
-    };
-
-    // Get for the past 7 days
-    for i in 1..DAYS_TO_CHECK {
-        let date = latest_date - Duration::days(i);
-        match get_exchange_rate_from_date(&date).await {
-            Ok(map) => rates.push(map),
-            Err(err) => return Err(err),
-        }
-    }
-
-    return Ok(rates);
 }
